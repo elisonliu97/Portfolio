35811bb7a2994267231fe1ed1edef96922d179eb
Elison <elisonliu97@gmail.com>
2021-09-10 21:08:31 -0400

    removed fadein on page load due to conflicts with hashrouter

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Projects from './components/Projects/Projects'
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 
 function App() {
@@ -13,19 +12,11 @@ function App() {
     <Router>
       <NavBar />
       <Route render={({ location }) => (
-        <TransitionGroup>
-          <CSSTransition
-            key={location.key}
-            timeout={0}
-            classNames="fade"
-          >
-            <Switch location={location}>
-              <Route path="/" exact component={Home} />
-              <Route path="/about" component={About} />
-              <Route path="/projects" component={Projects} />
-            </Switch>
-          </CSSTransition>
-        </TransitionGroup>
+        <Switch location={location}>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+        </Switch>
       )} />
     </Router>
   );
